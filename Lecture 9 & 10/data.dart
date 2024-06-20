91abdb8241bd26d2c687903cfe6f7ea71091e426
Jangle <onlyforparthcoding+github@gmail.com>
2024-06-21 00:48:34 +0530

    Added Wallpaper app content

---
+++ Lecture 9 & 10/data.dart
@@ -0,0 +1,5 @@
+List<String> data1 = ["assets/one.jpg", "assets/6.jpg"];
+List<String> data2 = [
+  "assets/3.jpg",
+  "assets/5.jpg",
+];
---
+++ Lecture 9 & 10/main.dart
@@ -0,0 +1,83 @@
+import "dart:typed_data";
+import 'package:image_gallery_saver/image_gallery_saver.dart';
+import "package:flutter/material.dart";
+import "package:flutter/services.dart";
+import "package:wallpaper_app/data.dart";
+
+void main() {
+  runApp(const WallpaperApp());
+}
+
+class WallpaperApp extends StatelessWidget {
+  const WallpaperApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    void downloadImage(String url) async {
+      final ByteData bytes = await rootBundle.load(url);
+      final Uint8List list = bytes.buffer.asUint8List();
+      final result = await ImageGallerySaver.saveImage(list);
+    }
+
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue[200],
+        appBar: AppBar(
+          backgroundColor: Colors.blueAccent,
+          title: const Text(
+            "Wallpaper App",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+              fontSize: 25,
+            ),
+          ),
+          centerTitle: true,
+        ),
+        body: ListView.builder(
+          itemCount: data1.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Column(
+              children: [
+                const SizedBox(
+                  height: 25,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        downloadImage(data1[index]);
+                      },
+                      child: Expanded(
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(30),
+                          child: Image.asset(
+                            data1[index],
+                          ),
+                        ),
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {
+                        downloadImage(data2[index]);
+                      },
+                      child: Expanded(
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(30),
+                          child: Image.asset(
+                            data2[index],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
